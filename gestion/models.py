b991f851fe2c45bc34aca1be9a680a1f125ce186
DanielaAlejandraMamaniSilva <danielaalejandramamanisilva@gmail.com>
2026-04-18 15:33:12 -0400

    dashboard Bienes y Servicios

--- gestion/models.py
+++ gestion/models.py
@@ -75,6 +75,7 @@ class Bitacora(models.Model):
         ('pendiente', 'PENDIENTE'),
         ('validado', 'VALIDADO'),
         ('rechazado', 'RECHAZADO'),
+        ('animalia', 'ANOMALÍA')
     )
     estado_validacion = models.CharField(max_length=20, choices=ESTADOS_VALIDACION, default='pendiente')
 
